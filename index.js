92a09867f1c47085ec56e7b8ec2fe0366c7ed1a5
safvan-husain <developer.safvan@gmail.com>
2024-02-05 13:28:39 +0530

    moving multer upload into run route

--- index.js
+++ index.js
@@ -8,43 +8,9 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/rundart", (req, res) => {
-  // Specify the path to your Dart script
-  const dartScriptPath =
-    "C:\\Users\\safva\\OneDrive\\Desktop\\starkin_projects\\recover-http\\Recovery\\lib\\script.dart".replaceAll(
-      "\\",
-      "/"
-    );
-  // console.log(process.env.PATH.replaceAll(";", "\n"));
-  //   const dartScriptPath = __dirname + '\\Recovery\\lib\\script.dart';
-  if (!fs.existsSync(dartScriptPath)) {
-    console.error("Dart script file does not exist");
-    return res.status(404).send("Dart script file not found");
-  }
-
-  // process.env.PATH += ";D:\\flutter_windows_3.13.9-stable\\flutter\\bin";
-  // Spawn a new Dart process
-  const dartProcess = spawn(
-    "D:\\flutter_windows_3.13.9-stable\\flutter\\bin\\dart.bat",
-    [dartScriptPath]
-  );
-
-  // Handle process events
-  dartProcess.stdout.on("data", (data) => {
-    console.log(`Dart script output: ${data}`);
-  });
-
-  dartProcess.stderr.on("error", (data) => {
-    console.error(`Error from Dart script: ${data}`);
-  });
-
-  dartProcess.on("close", (code) => {
-    console.log(`Dart script exited with code ${code}`);
-    res.send("Dart script executed successfully!");
-  });
-});
 
 const multer = require("multer");
+//I want to save the uploaded image with their agencyId, but I will only know it when they give request to /run route.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -106,17 +72,6 @@ app.get("/download", (req, res) => {
   res.download(file); // Set disposition and send it.
 });
 
-// app.post("/run", async (req, res) => {
-//   const { name } = req.body;
-//   await renameApp(name);
-//   await setAgencyDetails(name, '', 1, "918888888", "india");
-//   const child = fork("./long_task.js");
-//   var outputDir = ensureDirectoryExists(`apk/${name}`)
-//   child.send({ command: "start", outputDir });
-//   child.on("message", (result) => {
-//     res.json(result);
-//   });
-// });
 
 function ensureDirectoryExists(folderPath) {
   console.log(`folder path : ${folderPath}`);
